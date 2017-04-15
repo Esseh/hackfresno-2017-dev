963b7d72c2f22d8c0d11f70ea7e1fbbd2759001c
Esseh <kennethwilleford@gmail.com>
2017-04-15 05:02:33 -0700

    fixed bug in context

--- CONTEXT/CONTEXT_struct.go
+++ CONTEXT/CONTEXT_struct.go
@@ -18,7 +18,7 @@ func NewContext(res http.ResponseWriter,req *http.Request) S {
 	ctx_a := appengine.NewContext(req)
 	u := USER.S{}
 	c, err := req.Cookie("login")
-	if err != nil {
+	if err == nil {
 		userKey, _ := strconv.ParseInt(c.Value,10,64)
 		retrievable.GetEntity(ctx_a,userKey,&u)
 	}
