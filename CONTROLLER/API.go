c73f5f0864d8c47912a07a1e14737ae294bc864a
Esseh <kennethwilleford@gmail.com>
2017-04-15 01:14:39 -0700

    Implemented CONTROLLER and VIEW

---
+++ CONTROLLER/API.go
@@ -0,0 +1,30 @@
+package CONTROLLER
+import (
+	"time"
+	"strconv"
+	"github.com/Esseh/retrievable"
+	"github.com/Esseh/hackfresno-2017-dev/CONTEXT"
+	"github.com/Esseh/hackfresno-2017-dev/GROUPS"
+)
+
+func API(ctx CONTEXT.S){
+	g := &GROUPS.S{}
+	id, _ := strconv.ParseInt(ctx.Req.FormValue("groupID"),10,64)
+	retrievable.GetEntity(ctx,id,g)
+	var modified bool
+	switch ctx.Req.FormValue("control") {
+		case "makeEvent":	
+			g.Events(ctx).MakeEvent(ctx)
+			modified = true
+		case "makeThread":
+			g.Threads(ctx).MakeThread(ctx)
+			modified = true
+		case "makePost":
+			g.Threads(ctx).GetThread(ctx).MakePost(ctx)
+			modified = true
+	}
+	if modified {
+		g.LastUpdated = time.Now()
+		retrievable.PlaceEntity(ctx,id,g)
+	}
+}
---
+++ VIEW/API.go
@@ -0,0 +1,27 @@
+package VIEW
+import (
+	"strconv"
+	"github.com/Esseh/retrievable"
+	"github.com/Esseh/hackfresno-2017-dev/CONTEXT"
+	"github.com/Esseh/hackfresno-2017-dev/GROUPS"
+)
+
+func API(ctx CONTEXT.S){
+	g := &GROUPS.S{}
+	id, _ := strconv.ParseInt(ctx.Req.FormValue("groupID"),10,64)
+	retrievable.GetEntity(ctx,id,g)
+	switch ctx.Req.FormValue("control") {
+		case "lastModified":	
+			g.LastModified(ctx)
+		case "about":
+			g.GetAbout(ctx)
+		case "events":
+			g.Events(ctx).GetEvents(ctx)
+		case "eventInfo":
+			g.Events(ctx).GetEvent(ctx).GetInfo(ctx)
+		case "threadTitles":
+			g.Threads(ctx).GetThreadTitles(ctx)
+		case "posts":
+			g.Threads(ctx).GetThread(ctx).GetPosts(ctx)
+	}
+}
